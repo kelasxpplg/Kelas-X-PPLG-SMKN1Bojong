6b2a315486c5270211804b347477984e9da037c0
kelasxpplg <kelasxpplgsmkn1bojong@gmail.com>
2026-08-12 09:52:52 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// GANTI isi di bawah ini dengan firebaseConfig dari project Firebase kamu.
+// Cara ambil: Firebase Console > Project Settings > scroll ke "Your apps" > pilih app web kamu
+
+const firebaseConfig = {
+  apiKey: "ISI_DI_SINI",
+  authDomain: "ISI_DI_SINI",
+  projectId: "ISI_DI_SINI",
+  storageBucket: "ISI_DI_SINI",
+  messagingSenderId: "ISI_DI_SINI",
+  appId: "ISI_DI_SINI"
+};
+
+firebase.initializeApp(firebaseConfig);
